542314b1e1d618b098635276b281ddc01ea10fb7
xzci <xzci@outlook.com>
2017-09-15 12:56:52 +0800

    "the past work"

---
+++ TwoSum-01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) 
+	{
+		vector <int> answer;
+		for (auto i = 0; i != nums.size(); i++)
+		{
+			for (auto j = i + 1; j != nums.size(); j++)
+			{
+				if (nums[i] + nums[j] == target)
+				{
+					answer.push_back(i);
+					answer.push_back(j);
+				}
+			}	
+		}
+		return answer;
+	}
+};
+
+
